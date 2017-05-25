e7a67479cc72a549751133c4f0d6a02c7687f518
AFinalStone <602392033@qq.com>
2017-05-25 11:03:46 +0800

    实现第三方登录返回用户信息

--- loginByWX/src/main/java/com/example/login/LoginActivity.java
+++ loginByWX/src/main/java/com/example/login/LoginActivity.java
@@ -7,9 +7,17 @@ import android.view.View;
 
 import com.example.loginbytencent.R;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.tencent.qq.QQ;
+import cn.sharesdk.wechat.friends.Wechat;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -33,9 +41,53 @@ public class LoginActivity extends AppCompatActivity {
 
                 break;
             case R.id.btn_loginByQQ:
+                loginQQ(QQ.NAME);
                 break;
             case R.id.btn_loginByWx:
+                loginQQ(Wechat.NAME);
                 break;
         }
+    }
+
+    private void loginQQ(String typeName) {
+        ShareSDK.initSDK(this);
+        Platform weibo = ShareSDK.getPlatform(typeName);
+        weibo.SSOSetting(false);  //设置false表示使用SSO授权方式
+        //回调信息，可以在这里获取基本的授权返回的信息，但是注意如果做提示和UI操作要传到主线程handler里去执行
+        weibo.setPlatformActionListener(new PlatformActionListener() {
+
+            @Override
+            public void onError(Platform arg0, int arg1, Throwable arg2) {
+                // TODO Auto-generated method stub
+                arg2.printStackTrace();
+                etName.setText("错误信息"+arg2.getMessage());
+            }
+
+            @Override
+            public void onComplete(Platform arg0, int arg1, HashMap<String, Object> arg2) {
+                // TODO Auto-generated method stub
+                //输出所有授权信息
+//                arg0.getDb().exportData();
+                //采用Iterator遍历HashMap
+                Iterator it = arg2.keySet().iterator();
+                while (it.hasNext()) {
+                    String key = (String) it.next();
+                    System.out.println("key:" + key);
+                    System.out.println("value:" + arg2.get(key));
+                }
+
+            }
+
+            @Override
+            public void onCancel(Platform arg0, int arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        //authorize与showUser单独调用一个即可
+//        weibo.authorize();//单独授权,OnComplete返回的hashmap是空的
+        weibo.showUser(null);//授权并获取用户信息
+        //移除授权
+        //weibo.removeAccount(true);
     }
 }
